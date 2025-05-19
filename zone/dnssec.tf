d8aa73c94a3c1691825a301509e71e9a5b567030
Rashed Obaid <mail@rashedobaid.com>
2025-05-19 21:42:56 +0800

    Fix dnssec resource

--- zone/dnssec.tf
+++ zone/dnssec.tf
@@ -6,20 +6,20 @@ resource "cloudflare_zone_dnssec" "dnssec" {
   dnssec_presigned    = var.dnssec_presigned
 }
 
-resource "cloudflare_dns_record" "dnssec" {
-  count = length(cloudflare_zone_dnssec.dnssec) > 0 ? 1 : 0
+# resource "cloudflare_dns_record" "dnssec" {
+#   count = length(cloudflare_zone_dnssec.dnssec) > 0 ? 1 : 0
 
-  zone_id = cloudflare_zone.domain.id
-  name    = cloudflare_zone.domain.name
-  type    = "DS"
-  ttl     = 1
-  data = {
-    key_tag     = cloudflare_zone_dnssec.dnssec[0].key_tag
-    algorithm   = cloudflare_zone_dnssec.dnssec[0].algorithm
-    digest_type = cloudflare_zone_dnssec.dnssec[0].digest_type
-    digest      = cloudflare_zone_dnssec.dnssec[0].digest
-  }
-  comment = "Cloudflare DNSSEC"
+#   zone_id = cloudflare_zone.domain.id
+#   name    = cloudflare_zone.domain.name
+#   type    = "DS"
+#   ttl     = 1
+#   data = {
+#     key_tag     = cloudflare_zone_dnssec.dnssec[0].key_tag
+#     algorithm   = cloudflare_zone_dnssec.dnssec[0].algorithm
+#     digest_type = cloudflare_zone_dnssec.dnssec[0].digest_type
+#     digest      = cloudflare_zone_dnssec.dnssec[0].digest
+#   }
+#   comment = "Cloudflare DNSSEC"
 
-  depends_on = [cloudflare_zone_dnssec.dnssec]
-}
+#   depends_on = [cloudflare_zone_dnssec.dnssec]
+# }
