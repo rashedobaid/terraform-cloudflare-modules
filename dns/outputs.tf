cd5c24132667e55da8a4c67d5cc25996020fdb21
Rashed Obaid <mail@rashedobaid.com>
2025-05-19 18:04:27 +0800

    Fix outputs

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -1,5 +1,5 @@
 output "record_hostnames_to_ids" {
-  description = "A map of the zone record hostnames to IDs."
+  description = "A map of the zone record key to IDs."
   value = {
     for record_key, record in cloudflare_dns_record.records :
     record_key => record.id
