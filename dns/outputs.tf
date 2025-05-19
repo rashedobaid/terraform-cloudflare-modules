625f76593ec50c2ddde869f817630d78b69eb49a
Rashed Obaid <mail@rashedobaid.com>
2025-05-19 20:26:07 +0800

    Revert outputs

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -1,11 +1,3 @@
-output "resource_addresses_to_ids" {
-  description = "Map of resource addresses to Cloudflare record IDs, with quoted keys."
-  value = {
-    for record_key, record in cloudflare_dns_record.records :
-    "cloudflare_dns_record.records[" + "\"" + record_key + "\"" + "]" => record.id
-  }
-}
-
 output "dns_record_ids" {
   description = "A map of logical DNS record keys (as defined in the input variable or generated key) to their Cloudflare-assigned DNS record IDs."
   value = {
