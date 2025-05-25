210c8c8442ddbdeab89f0ae9e3f856c4e7c43e39
Rashed Obaid <mail@rashedobaid.com>
2025-05-25 12:20:20 +0800

    Add zone output

--- zone/outputs.tf
+++ zone/outputs.tf
@@ -2,6 +2,10 @@ output "zone_id" {
   value = cloudflare_zone.domain.id
 }
 
+output "zone" {
+  value = cloudflare_zone.domain.name
+}
+
 output "name_servers" {
   value = cloudflare_zone.domain.name_servers
 }
