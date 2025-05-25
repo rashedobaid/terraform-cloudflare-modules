8a034a922a04a18625dba7e56c7c698134421e64
Rashed Obaid <mail@rashedobaid.com>
2025-05-25 13:35:19 +0800

    Remove read only ID for zone settings

--- zone/main.tf
+++ zone/main.tf
@@ -8,6 +8,5 @@ resource "cloudflare_zone" "domain" {
 resource "cloudflare_zone_setting" "domain" {
   zone_id    = cloudflare_zone.domain.id
   setting_id = "ssl"
-  id         = "ssl"
   value      = var.ssl_level
 }
